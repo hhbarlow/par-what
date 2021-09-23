93add6a9f21d072aae7ae3814980d363287fec1f
hhbarlow <hilaryhbarlow@gmail.com>
2021-09-22 21:14:48 -0400

    improvements

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
-// weeklyParshah <- GET(
-//     url = "https://www.sefaria.org/api/calendars"
-// )
+const titleEng = document.getElementById('name-en')
+const titleHeb = document.getElementById('name-he')
 
-// content(weeklyParshah, type = "text")
-
-// async function getParshah(){
-//     const calendarUrl = 'https://www.sefaria.org/api/calendars/'
-    
-    // const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
-    // const proxyUrl = 'https://shrouded-inlet-14671.herokuapp.com/'
-
-//     try {
-
-//     const response = await fetch(calendarUrl)
-//     const data = await response.json()
-//         console.log(data)
-  
-
-//     } catch (error) {
-//         getParshah()
-//         console.log('Omg what???', error)
-//     }
-// }
-
-import fetch from 'node-fetch'
+// import fetch from 'node-fetch'
 
 async function getParshah(){
     const calendarUrl = 'https://www.sefaria.org/api/calendars/'
@@ -31,17 +9,22 @@ async function getParshah(){
     try {
     const response = await fetch(calendarUrl);
     const data = await response.json();
-        console.log(data['calendar_items']['0'])
+    console.log(data)
+        // console.log(data['calendar_items']['0'])
+        console.log(data['calendar_items']['0']['title']['en'])
         // console.log(data['calendar_items']['displayValue']);
         // console.log(data['calendar_items']['displayValue']['he']);
         // console.log(data['calendar_items']['url']);
         // console.log(data['calendar_items']['title']['en']);
         // console.log(data['calendar_items']['title']['he']);
-    if(!data) {
+        titleEng.innerText =  data['calendar_items']['0']['title']['en']
+    
+        if(!data) {
         return;
     }
     }
     
+
     catch (error) {
         // getParshah()
         console.log('omg an error', error)
