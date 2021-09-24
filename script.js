8c4ab14fb3031dfdf1d4d6f9d3e42d0304a0db6c
hhbarlow <hilaryhbarlow@gmail.com>
2021-09-24 03:35:49 -0400

    structuring things better

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const titleEng = document.getElementById('name-en')
 const titleHeb = document.getElementById('name-he')
+const holidayTitleEng = document.getElementById('holiday-title-en')
+const holidayTitleHeb = document.getElementById('holiday-title-he')
+const refEng = document.getElementById('ref-en')
+const refHeb = document.getElementById('ref-he')
+const descEng = document.getElementById('desc-en')
+const descHeb = document.getElementById('desc-he')
 
 // import fetch from 'node-fetch'
 
@@ -9,15 +15,25 @@ async function getParshah(){
     try {
     const response = await fetch(calendarUrl);
     const data = await response.json();
+    
     console.log(data)
-        // console.log(data['calendar_items']['0'])
-        console.log(data['calendar_items']['0']['title']['en'])
-        // console.log(data['calendar_items']['displayValue']);
-        // console.log(data['calendar_items']['displayValue']['he']);
-        // console.log(data['calendar_items']['url']);
-        // console.log(data['calendar_items']['title']['en']);
-        // console.log(data['calendar_items']['title']['he']);
-        titleEng.innerText =  data['calendar_items']['0']['title']['en']
+    console.log(data['calendar_items']['0']['title']['en'])
+    console.log(data['calendar_items']['0']['title']['he'])
+    console.log(data['calendar_items']['0']['displayValue']['en'])
+    console.log(data['calendar_items']['0']['displayValue']['he'])
+    console.log(data['calendar_items']['0']['ref'])
+    console.log(data['calendar_items']['0']['heRef'])
+    console.log(data['calendar_items']['0']['description']['en'])
+    console.log(data['calendar_items']['0']['description']['he'])
+        
+    titleEng.innerText = data['calendar_items']['0']['title']['en'];
+    titleHeb.innerText = data['calendar_items']['0']['title']['he']
+    holidayTitleEng.innerText = data['calendar_items']['0']['displayValue']['en']
+    holidayTitleHeb.innerText = data['calendar_items']['0']['displayValue']['he']
+    refEng.innerText = data['calendar_items']['0']['ref']
+    refHeb.innerText = data['calendar_items']['0']['heRef']
+    descEng.innerText = data['calendar_items']['0']['description']['en']
+    descHeb.innerText = data['calendar_items']['0']['description']['he']
     
         if(!data) {
         return;
